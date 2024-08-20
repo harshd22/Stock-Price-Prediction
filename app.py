f00aadca3ec7124af810e174d54061441560aa23
harshd22 <136061795+harshd22@users.noreply.github.com>
2024-08-20 15:09:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,23 +11,24 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsRegressor
 from xgboost import XGBRegressor
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.ensemble import RandomForestRegressor, ExtraTreesRegressor
 from sklearn.metrics import r2_score, mean_absolute_error
-import plotly.graph_objects as go  # For candlestick chart
+import plotly.graph_objects as go  # for candlestick chart
 
+# Title and sidebar information
 st.title('Stock Price Predictions')
 st.sidebar.info('Welcome to the Stock Price Prediction App. Choose your options below')
 st.sidebar.info("Created and designed by [Harsh Dugad](www.linkedin.com/in/harsh-dugad-90067923b)")
 
+# Main function to handle app logic
 def main():
-    option = st.sidebar.selectbox('Make a choice', ['Visualize', 'Candlestick', 'Recent Data', 'Predict'])
+    option = st.sidebar.selectbox('Make a choice', ['Visualize','Recent Data', 'Candlestick', 'Predict'])
     if option == 'Visualize':
         tech_indicators()
-    elif option == 'Candlestick':
-        candlestick_chart()
     elif option == 'Recent Data':
         dataframe()
+    elif option == 'Candlestick':
+        candlestick_chart()
     else:
         predict()
 
@@ -36,6 +37,7 @@ def download_data(op, start_date, end_date):
     df = yf.download(op, start=start_date, end=end_date, progress=False)
     return df
 
+# Sidebar input fields
 option = st.sidebar.text_input('Enter a Stock Symbol', value='SPY')
 option = option.upper()
 today = datetime.date.today()
@@ -44,114 +46,139 @@ before = today - datetime.timedelta(days=duration)
 start_date = st.sidebar.date_input('Start Date', value=before)
 end_date = st.sidebar.date_input('End date', today)
 
+# Download data based on inputs
 if st.sidebar.button('Send'):
     if start_date < end_date:
         st.sidebar.success(f'Start date: {start_date}\n\nEnd date: {end_date}')
-        download_data(option, start_date, end_date)
+        data = download_data(option, start_date, end_date)
+        if data.empty:
+            st.sidebar.error(f'No data found for symbol `{option}`')
+        else:
+            st.sidebar.success(f'Data successfully retrieved for `{option}`')
     else:
         st.sidebar.error('Error: End date must fall after start date')
 
+# Download the data globally for access in different functions
 data = download_data(option, start_date, end_date)
 scaler = StandardScaler()
 
+# Technical indicators visualization
 def tech_indicators():
     st.header('Technical Indicators')
-    option = st.radio('Choose a Technical Indicator to Visualize', ['Close', 'BB', 'MACD', 'RSI', 'SMA', 'EMA'])
+    if not data.empty:
+        option = st.radio('Choose a Technical Indicator to Visualize', ['Close', 'BB', 'MACD', 'RSI', 'SMA', 'EMA'])
 
-    # Bollinger bands
-    bb_indicator = BollingerBands(data.Close)
-    bb = data
-    bb['bb_h'] = bb_indicator.bollinger_hband()
-    bb['bb_l'] = bb_indicator.bollinger_lband()
-    # Creating a new dataframe
-    bb = bb[['Close', 'bb_h', 'bb_l']]
-    # MACD
-    macd = MACD(data.Close).macd()
-    # RSI
-    rsi = RSIIndicator(data.Close).rsi()
-    # SMA
-    sma = SMAIndicator(data.Close, window=14).sma_indicator()
-    # EMA
-    ema = EMAIndicator(data.Close).ema_indicator()
+        # Bollinger bands
+        bb_indicator = BollingerBands(data.Close)
+        bb = data.copy()
+        bb['bb_h'] = bb_indicator.bollinger_hband()
+        bb['bb_l'] = bb_indicator.bollinger_lband()
+        bb = bb[['Close', 'bb_h', 'bb_l']]
 
-    if option == 'Close':
-        st.write('Close Price')
-        st.line_chart(data.Close)
-    elif option == 'BB':
-        st.write('BollingerBands')
-        st.line_chart(bb)
-    elif option == 'MACD':
-        st.write('Moving Average Convergence Divergence')
-        st.line_chart(macd)
-    elif option == 'RSI':
-        st.write('Relative Strength Indicator')
-        st.line_chart(rsi)
-    elif option == 'SMA':
-        st.write('Simple Moving Average')
-        st.line_chart(sma)
+        # MACD
+        macd = MACD(data.Close).macd()
+        # RSI
+        rsi = RSIIndicator(data.Close).rsi()
+        # SMA
+        sma = SMAIndicator(data.Close, window=14).sma_indicator()
+        # EMA
+        ema = EMAIndicator(data.Close).ema_indicator()
+
+        if option == 'Close':
+            st.write('Close Price')
+            st.line_chart(data.Close)
+        elif option == 'BB':
+            st.write('BollingerBands')
+            st.line_chart(bb)
+        elif option == 'MACD':
+            st.write('Moving Average Convergence Divergence')
+            st.line_chart(macd)
+        elif option == 'RSI':
+            st.write('Relative Strength Indicator')
+            st.line_chart(rsi)
+        elif option == 'SMA':
+            st.write('Simple Moving Average')
+            st.line_chart(sma)
+        else:
+            st.write('Exponential Moving Average')
+            st.line_chart(ema)
     else:
-        st.write('Exponential Moving Average')
-        st.line_chart(ema)
+        st.write('No data available to visualize.')
 
-def candlestick_chart():
-    st.header('Candlestick Chart')
-    fig = go.Figure(data=[go.Candlestick(x=data.index,
-                                         open=data['Open'],
-                                         high=data['High'],
-                                         low=data['Low'],
-                                         close=data['Close'])])
-    fig.update_layout(title=f'Candlestick Chart for {option}', xaxis_title='Date', yaxis_title='Price',
-                      xaxis_rangeslider_visible=False)
-    st.plotly_chart(fig)
-
+# Display recent data
 def dataframe():
     st.header('Recent Data')
-    st.dataframe(data.tail(10))
+    if not data.empty:
+        st.dataframe(data.tail(10))
+    else:
+        st.write('No data available to display.')
 
+# Candlestick chart visualization
+def candlestick_chart():
+    st.header('Candlestick Chart')
+    if not data.empty:
+        fig = go.Figure(data=[go.Candlestick(x=data.index,
+                                             open=data['Open'],
+                                             high=data['High'],
+                                             low=data['Low'],
+                                             close=data['Close'])])
+        fig.update_layout(title=f'Candlestick chart for {option}',
+                          xaxis_title='Date',
+                          yaxis_title='Price',
+                          xaxis_rangeslider_visible=False)
+        st.plotly_chart(fig)
+    else:
+        st.write('No data available for candlestick chart.')
+
+# Prediction function
 def predict():
-    model = st.radio('Choose a model', ['LinearRegression', 'RandomForestRegressor', 'ExtraTreesRegressor', 'KNeighborsRegressor', 'XGBoostRegressor'])
-    num = st.number_input('How many days forecast?', value=5)
-    num = int(num)
-    if st.button('Predict'):
-        if model == 'LinearRegression':
-            engine = LinearRegression()
-            model_engine(engine, num)
-        elif model == 'RandomForestRegressor':
-            engine = RandomForestRegressor()
-            model_engine(engine, num)
-        elif model == 'ExtraTreesRegressor':
-            engine = ExtraTreesRegressor()
-            model_engine(engine, num)
-        elif model == 'KNeighborsRegressor':
-            engine = KNeighborsRegressor()
-            model_engine(engine, num)
-        else:
-            engine = XGBRegressor()
-            model_engine(engine, num)
+    if not data.empty:
+        model = st.radio('Choose a model', ['LinearRegression', 'RandomForestRegressor', 'ExtraTreesRegressor', 'KNeighborsRegressor', 'XGBoostRegressor'])
+        num = st.number_input('How many days forecast?', value=5)
+        num = int(num)
+        if st.button('Predict'):
+            if model == 'LinearRegression':
+                engine = LinearRegression()
+                model_engine(engine, num)
+            elif model == 'RandomForestRegressor':
+                engine = RandomForestRegressor()
+                model_engine(engine, num)
+            elif model == 'ExtraTreesRegressor':
+                engine = ExtraTreesRegressor()
+                model_engine(engine, num)
+            elif model == 'KNeighborsRegressor':
+                engine = KNeighborsRegressor()
+                model_engine(engine, num)
+            else:
+                engine = XGBRegressor()
+                model_engine(engine, num)
+    else:
+        st.write('No data available to make predictions.')
 
+# Model engine for predictions
 def model_engine(model, num):
-    # Getting only the closing price
+    # getting only the closing price
     df = data[['Close']]
-    # Shifting the closing price based on number of days forecast
+    # shifting the closing price based on number of days forecast
     df['preds'] = data.Close.shift(-num)
-    # Scaling the data
+    # scaling the data
     x = df.drop(['preds'], axis=1).values
     x = scaler.fit_transform(x)
-    # Storing the last num_days data
+    # storing the last num_days data
     x_forecast = x[-num:]
-    # Selecting the required values for training
+    # selecting the required values for training
     x = x[:-num]
-    # Getting the preds column
+    # getting the preds column
     y = df.preds.values
-    # Selecting the required values for training
+    # selecting the required values for training
     y = y[:-num]
 
     # Splitting the data
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=7)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.2, random_state=7)
     # Training the model
     model.fit(x_train, y_train)
     preds = model.predict(x_test)
-    st.text(f'r2_score: {r2_score(y_test, preds)}\nMAE: {mean_absolute_error(y_test, preds)}')
+    st.text(f'r2_score: {r2_score(y_test, preds)} \nMAE: {mean_absolute_error(y_test, preds)}')
     # Predicting stock price based on the number of days
     forecast_pred = model.predict(x_forecast)
     day = 1
